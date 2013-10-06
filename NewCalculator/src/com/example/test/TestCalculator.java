d8316075bdfe9b662eff16870207fe161d7b67a6
gces <gces@gces-MS-7592>
2013-10-06 08:12:19 +0545

    my third commit

---
+++ NewCalculator/src/com/example/test/TestCalculator.java
@@ -0,0 +1,28 @@
+package com.example.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.example.classes.Calculator;
+
+public class TestCalculator {
+	Calculator cal=new Calculator();
+
+	@Test
+	public void testadd() {
+		assertEquals("the output of addition is:",50,cal.add(10,40));	
+		//fail("Not yet implemented");
+	}
+	@Test
+	public void testmul() {
+		assertEquals("the output of multiply is:",50,cal.mul(10,40));	
+		//fail("Not yet implemented");
+	}
+	@Test
+	public void testdiv() {
+		assertEquals("the output of division is:",0,cal.div(10,40));	
+		//fail("Not yet implemented");
+	}
+
+}
